3651751934a25d17e11db97eed7f735ee03dcbdb
alex <asheet3@lsu.edu>
2025-05-10 09:25:58 -0500

    remove line

--- recursion/letter_combination_phone_number_17.py
+++ recursion/letter_combination_phone_number_17.py
@@ -8,7 +8,6 @@ class Solution:
         m = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
         li = []
 
-        # define recursive function to recursively
         def check(st, digits):
             # if we have emptied the digits string append created string
             if len(digits) == 0:
